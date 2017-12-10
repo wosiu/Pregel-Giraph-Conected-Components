5c1f136e4998b2864af94525a9357b4c747caad2
Michal Wos <mw792829570@gmail.com>
2017-12-10 22:26:44 +0100

    debug script

---
+++ debug.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+exec > >(tee -i debug.log)
+exec 2>&1
+
+pushd `dirname $0` > /dev/null
+PROJECT_HOME=`pwd -P`
+popd > /dev/null
+
+echo "=============MAVEN==========="
+which mvn
+ls -l `which mvn`
+mvn --version
+echo "=============JAVA============"
+which java
+ls -l `which java`
+java -version
+echo "=============BUILD==========="
+
+pushd $PROJECT_HOME/giraph-cc
+    mvn -e -X -Phadoop_2 -Dhadoop.version=2.8.2 -DskipTests clean package || { echo "Error while building"; exit 1; }
+popd
+
+
